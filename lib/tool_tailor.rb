7d4a7e8519280b91735ae72f3a2ef22ea34b6ed7
Kieran Klaassen <kieranklaassen@gmail.com>
2024-05-22 21:41:09 -0500

    Refactor method to handle UnboundMethod

--- lib/tool_tailor.rb
+++ lib/tool_tailor.rb
@@ -1,13 +1,13 @@
 require "tool_tailor/version"
-require 'json'
-require 'yard'
+require "json"
+require "yard"
 
 module ToolTailor
   class Error < StandardError; end
 
   # Converts a function to a JSON schema representation.
   #
-  # @param function [Method] The function to convert.
+  # @param function [Method, UnboundMethod] The function to convert.
   # @return [String] The JSON schema representation of the function.
   # @raise [ArgumentError] If the provided object is not a Method or UnboundMethod.
   def self.convert(function)
@@ -18,14 +18,13 @@ module ToolTailor
     file_path, line_number = function.source_location
     YARD.parse(file_path)
 
-    # Construct the correct identifier for the YARD object
     method_path = "#{function.owner}##{function.name}"
     yard_object = YARD::Registry.at(method_path)
     raise "Documentation for #{method_path} not found." if yard_object.nil?
 
     function_description = yard_object.docstring
 
-    parameters = yard_object.tags('param').map do |tag|
+    parameters = yard_object.tags("param").map do |tag|
       {
         name: tag.name,
         type: type_mapping(tag.types.first),
@@ -57,33 +56,60 @@ module ToolTailor
 
   # Maps Ruby types to JSON schema types.
   #
-  # @param type [Class] The Ruby type to map.
+  # @param type [String] The Ruby type to map.
   # @return [String] The corresponding JSON schema type.
   # @raise [ArgumentError] If the provided type is not supported.
   def self.type_mapping(type)
     case type
-    when "String"
-      'string'
-    when "Integer"
-      'integer'
-    when "Float"
-      'number'
-    when "TrueClass", "FalseClass"
-      'boolean'
-    when "Array"
-      'array'
-    when "Hash"
-      'object'
-    when "NilClass"
-      'null'
+    when "String"    then "string"
+    when "Integer"   then "integer"
+    when "Float"     then "number"
+    when "TrueClass", "FalseClass" then "boolean"
+    when "Array"     then "array"
+    when "Hash"      then "object"
+    when "NilClass"  then "null"
     else
-      # raise ArgumentError, "Unsupported type: #{type} #{type.class}"
-      'string'
+      raise ArgumentError, "Unsupported type: #{type} #{type.class}"
     end
   end
 end
 
 class UnboundMethod
+  # Converts an UnboundMethod to a JSON schema.
+  #
+  # @example
+  #   class ExampleClass
+  #     # @param name [String] The name of the person.
+  #     # @param age [Integer] The age of the person.
+  #     def greet(name, age)
+  #       puts "Hello, #{name}! You are #{age} years old."
+  #     end
+  #   end
+  #
+  #   ExampleClass.instance_method(:greet).to_json_schema
+  #   # => {
+  #   #   "type" => "function",
+  #   #   "function" => {
+  #   #     "name" => "greet",
+  #   #     "description" => "",
+  #   #     "parameters" => {
+  #   #       "type" => "object",
+  #   #       "properties" => {
+  #   #         "name" => {
+  #   #           "type" => "string",
+  #   #           "description" => "The name of the person."
+  #   #         },
+  #   #         "age" => {
+  #   #           "type" => "integer",
+  #   #           "description" => "The age of the person."
+  #   #         }
+  #   #       },
+  #   #       "required" => ["name", "age"]
+  #   #     }
+  #   #   }
+  #   # }
+  #
+  # @return [String] The JSON schema representation of the method.
   def to_json_schema
     ToolTailor.convert(self)
   end
